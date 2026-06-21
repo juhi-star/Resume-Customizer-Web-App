72b8c5c51ceeefa2b21663c5b783d2ce739f5447
juhi <your-email@gmail.com>
2026-06-21 12:44:47 +0530

    Rename backend/main.py to api.py to fix circular import in root main.py

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ backend = os.path.join(os.path.dirname(__file__), "backend")
 os.chdir(backend)
 sys.path.insert(0, backend)
 
-import main as backend_main
-app = backend_main.app
+from api import app
